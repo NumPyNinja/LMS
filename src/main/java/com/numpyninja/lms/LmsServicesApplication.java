38f4a6fd8397b90de1bb6afa960f0aeaf2f831c0
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2024-02-09 10:28:35 -0600

    Update LmsServicesApplication.java

--- src/main/java/com/numpyninja/lms/LmsServicesApplication.java
+++ src/main/java/com/numpyninja/lms/LmsServicesApplication.java
@@ -35,7 +35,7 @@ public class LmsServicesApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 				.allowedOrigins(frontEndURL)
-					.allowedOrigins("https://lms-frontend.herokuapp.com/login")
+					.allowedOrigins("https://lms-frontend.herokuapp.com/")
 						.allowedOrigins("http://localhost:4200")
 				.allowedMethods("GET","POST","PUT","PATCH","DELETE");
 			}
